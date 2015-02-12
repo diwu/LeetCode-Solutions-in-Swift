d917eb6cb85f19293301c2f5cee6e2ec60fa3b82
Di Wu <di.wu@dolby.com>
2015-02-12 17:08:48 +0800

    file rename

--- Medium_001_Two_Sum.playground/section-1.swift
+++ Medium_001_Two_Sum.playground/section-1.swift
@@ -4,6 +4,8 @@ https://oj.leetcode.com/problems/two-sum/
 
 #1 Two Sum
 
+Level: medium
+
 Given an array of integers, find two numbers such that they add up to a specific target number.
 
 The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2. Please note that your returned answers (both index1 and index2) are not zero-based.
