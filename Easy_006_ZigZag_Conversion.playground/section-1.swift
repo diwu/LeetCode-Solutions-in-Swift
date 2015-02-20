1463ac93d81a4957e02ee462dac5e6101d83b20e
Di Wu <diwufet@gmail.com>
2015-02-20 19:57:35 +0800

    006 ZigZag Conversion

---
+++ Easy_006_ZigZag_Conversion.playground/section-1.swift
@@ -0,0 +1,53 @@
+/*
+
+https://oj.leetcode.com/problems/zigzag-conversion/
+
+#6 ZigZag Conversion
+
+Level: easy
+
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string text, int nRows);
+
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+Inspired by @dylan_yu  at https://oj.leetcode.com/discuss/10493/easy-to-understand-java-solution
+
+*/
+
+// Helper
+extension String {
+    subscript (index: Int) -> Character {
+        return self[advance(self.startIndex, index)]
+    }
+}
+
+// O (N)
+func convert(s: String, nRows: Int) -> String {
+    var arr = Array<String>(count: nRows, repeatedValue: String())
+    var i = 0, len = count(s)
+    while i < len {
+        for var index = 0; index < nRows && i < len; index++ {
+            arr[index].append(s[i++])
+        }
+        for var index = nRows - 2; index > 0 && i < len; index-- {
+            arr[index].append(s[i++])
+        }
+    }
+    var res = String()
+    for i in 0..<nRows {
+        res.extend(arr[i])
+    }
+    return res
+}
+
+convert("PAYPALISHIRING", 3)
--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 
 
 ## LeetCode Solutions in Swift
-###All solutions are tested in the Playgournd of Xcode 6.3 Beta with Swift 1.2
-### Progress so far: 5 / 179 = 2.8%
+###All solutions are tested in Xcode 6.3 Beta with Swift 1.2
+### Progress: 6 / 179 = 3.4%
 
 1. Medium - [Two Sum](https://oj.leetcode.com/problems/two-sum/) 
 2. Medium - [Add Two Numbers](https://oj.leetcode.com/problems/add-two-numbers/) 
 3. Medium - [Longest Substring Without Repeating Characters ](https://oj.leetcode.com/problems/longest-substring-without-repeating-characters/)
 4. Hard - [Median of Two Sorted Arrays](https://oj.leetcode.com/problems/median-of-two-sorted-arrays/)
 5. Medium - [Longest Palindromic Substring](https://oj.leetcode.com/problems/longest-palindromic-substring/)
+6. Easy - [ZigZag Conversion](https://oj.leetcode.com/problems/zigzag-conversion/)
 
