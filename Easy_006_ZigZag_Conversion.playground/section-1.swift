3302f6d9fcbd70f7c064fe5070522909a6f4dbca
Di Wu <diwufet@gmail.com>
2015-03-06 16:49:17 +0800

    code fix

--- Easy_006_ZigZag_Conversion.playground/section-1.swift
+++ Easy_006_ZigZag_Conversion.playground/section-1.swift
@@ -1,4 +1,28 @@
-    
+/*
+
+https://oj.leetcode.com/problems/zigzag-conversion/
+
+#6 ZigZag Conversion
+
+Level: easy
+
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+
+P   A   H   N
+A P L S I I G
+Y   I   R
+
+And then read line by line: "PAHNAPLSIIGYIR"
+
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string text, int nRows);
+
+convert("PAYPALISHIRING", 3) should return "PAHNAPLSIIGYIR".
+
+Inspired by @dylan_yu at https://oj.leetcode.com/discuss/10493/easy-to-understand-java-solution
+
+*/
 
 // Helper
 extension String {
