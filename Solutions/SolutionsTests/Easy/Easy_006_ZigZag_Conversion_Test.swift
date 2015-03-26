1eed6a17912eeeac057f8f56a5a859a36ced1a72
Di Wu <di.wu@dolby.com>
2015-03-26 17:21:06 +0800

    unit test fix

--- Solutions/SolutionsTests/Easy/Easy_006_ZigZag_Conversion_Test.swift
+++ Solutions/SolutionsTests/Easy/Easy_006_ZigZag_Conversion_Test.swift
@@ -12,6 +12,6 @@ class Easy_006_ZigZag_Conversion_Test: XCTestCase {
     func testCase001() {
         var str1: String = Easy_006_ZigZag_Conversion.convert("PAYPALISHIRING", nRows:3)
         var str2: String = "PAHNAPLSIIGYIR"
-        XCTAssert(str1 == str2, "Pass")
+        XCTAssert(str1 == str2, "Easy_006_ZigZag_Conversion_Test #1")
     }
 }
--- Solutions/SolutionsTests/Medium/Medium_001_Two_Sum_Test.swift
+++ Solutions/SolutionsTests/Medium/Medium_001_Two_Sum_Test.swift
@@ -12,11 +12,11 @@ class Medium_001_Two_Sum_Test: XCTestCase {
     func testCase001() {
         var arr1: [Int] = Medium_001_Two_Sum.twoSum([2, 7, 11, 15], target: 9)
         var arr2: [Int] = [1, 2]
-        XCTAssert(arr1 == arr2, "Pass")
+        XCTAssert(arr1 == arr2, "Medium_001_Two_Sum_Test #1")
     }
     func testCase002() {
         var arr1: [Int] = Medium_001_Two_Sum.twoSum([5, 5], target: 10)
         var arr2: [Int] = [1, 2]
-        XCTAssert(arr1 == arr2, "Pass")
+        XCTAssert(arr1 == arr2, "Medium_001_Two_Sum_Test #2")
     }
 }
