7883c27ccd8097bd1e23336040105c8bba805548
Di Wu <di.wu@dolby.com>
2015-03-27 17:36:51 +0800

    put 005 into xcode project

---
+++ Solutions/Solutions/Medium/Medium_005_Longest_Palindromic_Substring.swift
@@ -0,0 +1,54 @@
+/*
+
+https://oj.leetcode.com/problems/longest-palindromic-substring/
+
+#5 Longest Palindromic Substring
+
+Level: medium
+
+Given a string S, find the longest palindromic substring in S. You may assume that the maximum length of S is 1000, and there exists one unique longest palindromic substring.
+
+Inspired by @hanleyChu  at https://oj.leetcode.com/discuss/9948/a-very-brief-o-n-time-o-1-space-solution-ac
+
+*/
+
+// Helper
+private extension String {
+    subscript (index: Int) -> Character {
+        var i: Index = advance(self.startIndex, index)
+        return self[i]
+    }
+    subscript (integerRange: Range<Int>) -> String {
+        return self[advance(startIndex, integerRange.startIndex)..<advance(startIndex, integerRange.endIndex+1, self.endIndex)]
+
+    }
+}
+
+class Medium_005_Longest_Palindromic_Substring {
+    // Helper
+    class func isPalindrome(s: String, startPosition: Int, endPosition: Int) -> Bool {
+        var len = endPosition - startPosition + 1
+        for i in 0..<len/2 {
+            if s[i + startPosition] != s[len - 1 - i + startPosition] {
+                return false
+            }
+        }
+        return true
+    }
+
+    // O (N ^ 2)
+    class func longest(s: String) -> String {
+        var longestLength = 0, longestIndex = 0, n = count(s)
+        var len = 0
+        for currentIndex in 0..<n {
+            if isPalindrome(s, startPosition: currentIndex - longestLength, endPosition: currentIndex) {
+                longestLength += 1
+                longestIndex = currentIndex
+            } else if currentIndex - longestLength - 1 >= 0 && isPalindrome(s, startPosition: currentIndex - longestLength - 1, endPosition: currentIndex) {
+                longestLength += 2
+                longestIndex = currentIndex
+            }
+        }
+        return s[longestIndex - longestLength + 1 ..< longestIndex]
+    }
+}
---
+++ Solutions/SolutionsTests/Medium/Medium_005_Longest_Palindromic_Substring_Test.swift
@@ -0,0 +1,65 @@
+//
+//  Medium_005_Longest_Palindromic_Substring_Test.swift
+//  Solutions
+//
+//  Created by Wu, Di on 3/27/15.
+//  Copyright (c) 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Medium_005_Longest_Palindromic_Substring_Test: XCTestCase {
+    func testCase001() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("a")
+        XCTAssert(v == "a", "Medium_005_Longest_Palindromic_Substring_Test #1")
+    }
+    func testCase002() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("aab")
+        XCTAssert(v == "aa", "Medium_005_Longest_Palindromic_Substring_Test #2")
+    }
+    func testCase003() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("abb")
+        XCTAssert(v == "bb", "Medium_005_Longest_Palindromic_Substring_Test #3")
+    }
+    func testCase004() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("abbc")
+        XCTAssert(v == "bb", "Medium_005_Longest_Palindromic_Substring_Test #4")
+    }
+    func testCase005() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("abbaaac")
+        XCTAssert(v == "abba", "Medium_005_Longest_Palindromic_Substring_Test #5")
+    }
+    func testCase006() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("aaaaaa")
+        XCTAssert(v == "aaaaaa", "Medium_005_Longest_Palindromic_Substring_Test #6")
+    }
+    func testCase007() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("abbbaaaaaa")
+        XCTAssert(v == "aaaaaa", "Medium_005_Longest_Palindromic_Substring_Test #7")
+    }
+    func testCase008() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("abbbaaaaaab")
+        XCTAssert(v == "baaaaaab", "Medium_005_Longest_Palindromic_Substring_Test #8")
+    }
+    func testCase009() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("abbbaaeeaab")
+        XCTAssert(v == "baaeeaab", "Medium_005_Longest_Palindromic_Substring_Test #9")
+    }
+    func testCase010() {
+        var v = Medium_005_Longest_Palindromic_Substring.longest("abbbaaeaab")
+        XCTAssert(v == "baaeaab", "Medium_005_Longest_Palindromic_Substring_Test #10")
+    }
+}
+
+/*
+longest("a")            //a
+longest("aab")          //aa
+longest("abb")          //bb
+longest("abbc")         //bb
+longest("abbaaac")      //abba
+longest("aaaaaa")       //aaaaaa
+longest("abbbaaaaaa")   //aaaaaa
+longest("abbbaaaaaab")  //baaaaaab
+longest("abbbaaeeaab")  //baaeeaab
+longest("abbbaaeaab")   //baaeaab
+*/
