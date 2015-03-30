94389abe72d91b0a28715cbb4d6d89c889bacbc9
Di Wu <di.wu@dolby.com>
2015-03-30 16:08:15 +0800

    XCTest assert helper

---
+++ Solutions/SolutionsTests/SolutionsTestsHelper.swift
@@ -0,0 +1,13 @@
+//
+//  SolutionsTestsHelper.swift
+//  Solutions
+//
+//  Created by Wu, Di on 3/30/15.
+//  Copyright (c) 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+func assertHelper(@autoclosure expression: () -> BooleanType, # problemName: String , # input: AnyObject, # resultValue: AnyObject, # expectedValue: AnyObject) {
+    XCTAssert(expression(), "\n====================================\nProblem: \(problemName)\n====================================\nInput: \"\(input)\"\n====================================\nExpected: \"\(expectedValue)\"\n====================================\nResult: \"\(resultValue)\"\n====================================\n")
+}
