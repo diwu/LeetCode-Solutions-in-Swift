e0b954534a1da25a33148c408819f69abe5d91a3
Di Wu <diwufet@gmail.com>
2015-04-16 18:58:41 +0800

    code clean

--- Solutions/SolutionsTests/Easy/Easy_009_Palindrome_Number_Test.swift
+++ Solutions/SolutionsTests/Easy/Easy_009_Palindrome_Number_Test.swift
@@ -9,33 +9,50 @@
 import XCTest
 
 class Easy_009_Palindrome_Number_Test: XCTestCase {
+
+    let ProbleName: String = "Easy_009_Palindrome_Number"
+
     func test_001() {
-        var v: Bool = Easy_009_Palindrome_Number.isPalindrome(-121)
-        XCTAssert(v == false, "Easy_009_Palindrome_Number_Test")
+        var input: Int = -121
+        var expected: Bool = false
+        var result = Easy_009_Palindrome_Number.isPalindrome(input)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
     func test_002() {
-        var v: Bool = Easy_009_Palindrome_Number.isPalindrome(121)
-        XCTAssert(v == true, "Easy_009_Palindrome_Number_Test")
+        var input: Int = 121
+        var expected: Bool = true
+        var result = Easy_009_Palindrome_Number.isPalindrome(input)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
     func test_003() {
-        var v: Bool = Easy_009_Palindrome_Number.isPalindrome(0)
-        XCTAssert(v == true, "Easy_009_Palindrome_Number_Test")
+        var input: Int = 0
+        var expected: Bool = true
+        var result = Easy_009_Palindrome_Number.isPalindrome(input)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
     func test_004() {
-        var v: Bool = Easy_009_Palindrome_Number.isPalindrome(Int.max)
-        XCTAssert(v == false, "Easy_009_Palindrome_Number_Test")
+        var input: Int = Int.max
+        var expected: Bool = false
+        var result = Easy_009_Palindrome_Number.isPalindrome(input)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
     func test_005() {
-        var v: Bool = Easy_009_Palindrome_Number.isPalindrome(Int.min)
-        XCTAssert(v == false, "Easy_009_Palindrome_Number_Test")
+        var input: Int = Int.min
+        var expected: Bool = false
+        var result = Easy_009_Palindrome_Number.isPalindrome(input)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
     func test_006() {
-        var v: Bool = Easy_009_Palindrome_Number.isPalindrome(1999999999999999999)
-        XCTAssert(v == false, "Easy_009_Palindrome_Number_Test")
+        var input: Int = 1999999999999999999
+        var expected: Bool = false
+        var result = Easy_009_Palindrome_Number.isPalindrome(input)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
     func test_007() {
-        var v: Bool = Easy_009_Palindrome_Number.isPalindrome(-1999999999999999999)
-        XCTAssert(v == false, "Easy_009_Palindrome_Number_Test")
+        var input: Int = -1999999999999999999
+        var expected: Bool = false
+        var result = Easy_009_Palindrome_Number.isPalindrome(input)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
 }
 
