5466f08ab8064e85aeefedcfa5f3c42b7f8198f8
Di Wu <diwufet@gmail.com>
2015-04-18 14:06:03 +0800

    test cases update

--- Solutions/SolutionsTests/Medium/Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.swift
+++ Solutions/SolutionsTests/Medium/Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.swift
@@ -16,4 +16,16 @@ class Medium_017_Letter_Combinations_Of_A_Phone_Number_Test: XCTestCase {
         var expected: Set<String> = Set(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
         assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
     }
+    func test_002() {
+        var input: String = ""
+        var result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
+        var expected: Set<String> = Set([""])
+        assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
+    }
+    func test_003() {
+        var input: String = "2"
+        var result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
+        var expected: Set<String> = Set(["a", "b", "c"])
+        assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
+    }
 }
