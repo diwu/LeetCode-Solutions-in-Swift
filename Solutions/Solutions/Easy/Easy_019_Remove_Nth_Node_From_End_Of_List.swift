41277251a1ab2375eece1e5cab7b5b5074f9a6d9
Di Wu <diwufet@gmail.com>
2015-04-20 12:03:59 +0800

    Easy 019

---
+++ Solutions/Solutions/Easy/Easy_019_Remove_Nth_Node_From_End_Of_List.swift
@@ -0,0 +1,51 @@
+/*
+
+https://github.com/diwu/LeetCode-Solutions-in-Swift
+
+#19 Remove Nth Node From End of List 
+
+Level: easy
+
+Given a linked list, remove the nth node from the end of list and return its head.
+
+For example,
+
+Given linked list: 1->2->3->4->5, and n = 2.
+
+After removing the second node from the end, the linked list becomes 1->2->3->5.
+
+Note:
+
+Given n will always be valid.
+Try to do this in one pass.
+
+Inspired by @i at https://leetcode.com/discuss/1656/is-there-a-solution-with-one-pass-and-o-1-space
+
+*/
+
+class Easy_019_Remove_Nth_Node_From_End_Of_List {
+
+    class Node {
+        var value: Int
+        var next: Node?
+        init(value: Int, next: Node?) {
+            self.value = value
+            self.next = next
+        }
+    }
+
+    class func removeNthFromEnd(head: Node?, n: Int) -> Node? {
+        var dummyHead: Node = Node(value: 0, next: head)
+        var fast: Node? = dummyHead, slow: Node? = dummyHead
+        var localN = n
+        while localN-- > 0 {
+            fast = fast?.next
+        }
+        while fast != nil && fast?.next != nil {
+            fast = fast?.next
+            slow = slow?.next
+        }
+        slow?.next = slow?.next?.next
+        return dummyHead.next
+    }
+}
---
+++ Solutions/SolutionsTests/Easy/Easy_019_Remove_Nth_Node_From_End_Of_List_Test.swift
@@ -0,0 +1,87 @@
+//
+//  Easy_019_Remove_Nth_Node_From_End_Of_List_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 4/20/15.
+//  Copyright (c) 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Easy_019_Remove_Nth_Node_From_End_Of_List_Test: XCTestCase {
+
+    static let ProblemName: String = "Easy_019_Remove_Nth_Node_From_End_Of_List"
+    static let TimeOutName = ProblemName + " Time Out"
+    static let TimeOut = 0.001
+
+    private typealias Node = Easy_019_Remove_Nth_Node_From_End_Of_List.Node
+
+    func test_001() {
+        var input: [AnyObject] = [[1, 2, 3, 4, 5], 2]
+        var expected: [Int] = [1, 2, 3, 5]
+        asyncHelper(input: input, expected: expected)
+    }
+
+    func test_002() {
+        var input: [AnyObject] = [[], 2]
+        var expected: [Int] = []
+        asyncHelper(input: input, expected: expected)
+    }
+
+    func test_003() {
+        var input: [AnyObject] = [[1, 2], 2]
+        var expected: [Int] = [2]
+        asyncHelper(input: input, expected: expected)
+    }
+
+    func test_004() {
+        var input: [AnyObject] = [[1, 2, 3], 3]
+        var expected: [Int] = [2, 3]
+        asyncHelper(input: input, expected: expected)
+    }
+
+    func test_005() {
+        var input: [AnyObject] = [[1], 1]
+        var expected: [Int] = []
+        asyncHelper(input: input, expected: expected)
+    }
+
+    private func helper1(intArray: [Int]) -> Node? {
+        var nodeArray: [Node] = []
+        for i in intArray {
+            var node: Node = Node(value: i, next: nil)
+            nodeArray.append(node)
+        }
+        if nodeArray.count == 0 {
+            return nil
+        }
+        for i in 0..<nodeArray.count-1 {
+            nodeArray[i].next = nodeArray[i+1]
+        }
+        return nodeArray[0]
+    }
+
+    private func helper2(head: Node?) -> [Int] {
+        var res: [Int] = []
+        var localHead = head
+        while localHead != nil {
+            res.append(localHead!.value)
+            localHead = localHead?.next
+        }
+        return res
+    }
+
+    func asyncHelper(# input: [AnyObject], expected: [Int]) {
+        var expectation: XCTestExpectation = self.expectationWithDescription(Easy_019_Remove_Nth_Node_From_End_Of_List_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var result: [Int] = self.helper2(Easy_019_Remove_Nth_Node_From_End_Of_List.removeNthFromEnd(self.helper1(input[0] as! [Int]), n: input[1] as! Int))
+            assertHelper(expected == result, problemName: Easy_019_Remove_Nth_Node_From_End_Of_List_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            expectation.fulfill()
+        })
+        waitForExpectationsWithTimeout(Easy_019_Remove_Nth_Node_From_End_Of_List_Test.TimeOut) { (error: NSError!) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Easy_019_Remove_Nth_Node_From_End_Of_List_Test.ProblemName, input: input, resultValue: Easy_019_Remove_Nth_Node_From_End_Of_List_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
