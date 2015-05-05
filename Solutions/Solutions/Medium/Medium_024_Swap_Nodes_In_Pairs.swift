59237b6dd7208e1ab8627902bd8544ed95dc23f3
Di Wu <diwufet@gmail.com>
2015-05-05 23:12:46 +0800

    medium 024

---
+++ Solutions/Solutions/Medium/Medium_024_Swap_Nodes_In_Pairs.swift
@@ -0,0 +1,44 @@
+/*
+
+https://leetcode.com/problems/swap-nodes-in-pairs/
+
+#24 Swap Nodes in Pairs
+
+Level: medium
+
+Given a linked list, swap every two adjacent nodes and return its head.
+
+For example,
+Given 1->2->3->4, you should return the list as 2->1->4->3.
+
+Your algorithm should use only constant space. You may not modify the values in the list, only nodes itself can be changed.
+
+Inspired by @mike3 at https://leetcode.com/discuss/3608/seeking-for-a-better-solution
+
+*/
+
+import Foundation
+
+class Medium_024_Swap_Nodes_In_Pairs {
+    class Node {
+        var value: Int
+        var next: Node?
+        init(value: Int, next: Node?) {
+            self.value = value
+            self.next = next
+        }
+    }
+    class func swap(# next1: Node, next2: Node) -> Node {
+        next1.next = next2.next
+        next2.next = next1
+        return next2
+    }
+    class func swapPairs(head: Node?) -> Node? {
+        var dummy: Node = Node(value: 0, next: nil)
+        dummy.next = head
+        for var curr: Node? = dummy; curr?.next != nil && curr?.next?.next != nil; curr = curr?.next?.next {
+            curr?.next = swap(next1: curr!.next!, next2: curr!.next!.next!)
+        }
+        return dummy.next
+    }
+}
---
+++ Solutions/SolutionsTests/Medium/Medium_024_Swap_Nodes_In_Pairs_Test.swift
@@ -0,0 +1,82 @@
+//
+//  Medium_024_Swap_Nodes_In_Pairs_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 5/5/15.
+//  Copyright (c) 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Medium_024_Swap_Nodes_In_Pairs_Test: XCTestCase {
+    private static let ProblemName: String = "Medium_024_Swap_Nodes_In_Pairs"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    private typealias Node = Medium_024_Swap_Nodes_In_Pairs.Node
+    func test_001() {
+        var input: Node? = helper1([1, 2, 3, 4])
+        var expected: [Int] = [2, 1, 4, 3]
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        var input: Node? = helper1([])
+        var expected: [Int] = []
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        var input: Node? = helper1([1])
+        var expected: [Int] = [1]
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        var input: Node? = helper1([1, 2])
+        var expected: [Int] = [2, 1]
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        var input: Node? = helper1([1, 2, 3])
+        var expected: [Int] = [2, 1, 3]
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_006() {
+        var input: Node? = helper1([1, 2, 3, 4, 5])
+        var expected: [Int] = [2, 1, 4, 3, 5]
+        asyncHelper(input: input, expected: expected)
+    }
+    private func helper1(intArray: [Int]) -> Node? {
+        var nodeArray: [Node] = []
+        for i in intArray {
+            var node: Node = Node(value: i, next: nil)
+            nodeArray.append(node)
+        }
+        if nodeArray.count == 0 {
+            return nil
+        }
+        for i in 0..<nodeArray.count-1 {
+            nodeArray[i].next = nodeArray[i+1]
+        }
+        return nodeArray[0]
+    }
+    private func helper2(head: Node?) -> [Int] {
+        var res: [Int] = []
+        var localHead = head
+        while localHead != nil {
+            res.append(localHead!.value)
+            localHead = localHead?.next
+        }
+        return res
+    }
+    private func asyncHelper(# input: Node?, expected: [Int]) {
+        var expectation: XCTestExpectation = self.expectationWithDescription(Medium_024_Swap_Nodes_In_Pairs_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var result: [Int] = self.helper2(Medium_024_Swap_Nodes_In_Pairs.swapPairs(input))
+            assertHelper(expected == result, problemName: Medium_024_Swap_Nodes_In_Pairs_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            expectation.fulfill()
+        })
+        waitForExpectationsWithTimeout(Medium_024_Swap_Nodes_In_Pairs_Test.TimeOut) { (error: NSError!) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Medium_024_Swap_Nodes_In_Pairs_Test.ProblemName, input: input, resultValue: Medium_024_Swap_Nodes_In_Pairs_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
