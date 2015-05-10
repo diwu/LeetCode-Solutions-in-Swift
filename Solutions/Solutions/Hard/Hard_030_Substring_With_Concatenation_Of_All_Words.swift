36b8ab4ecbd465515c59474cc57efe576b40f062
Di Wu <diwufet@gmail.com>
2015-05-10 17:34:30 +0800

    hard 030

---
+++ Solutions/Solutions/Hard/Hard_030_Substring_With_Concatenation_Of_All_Words.swift
@@ -0,0 +1,94 @@
+/*
+
+https://leetcode.com/problems/substring-with-concatenation-of-all-words/
+
+#30 Substring with Concatenation of All Words
+
+Level: hard
+
+You are given a string, s, and a list of words, words, that are all of the same length. Find all starting indices of substring(s) in s that is a concatenation of each word in words exactly once and without any intervening characters.
+
+For example, given:
+s: "barfoothefoobarman"
+words: ["foo", "bar"]
+
+You should return the indices: [0,9].
+(order does not matter).
+
+Inspired by @ramakanthd92 at https://leetcode.com/discuss/366/better-solution-than-brute-force
+
+*/
+
+import Foundation
+
+private extension String {
+    subscript (range: Range<Int>) -> String {
+        if range.endIndex > count(self) {
+            return self[advance(self.startIndex, range.startIndex)..<advance(self.startIndex, count(self))]
+        } else {
+            return self[advance(self.startIndex, range.startIndex)..<advance(self.startIndex, range.endIndex)]
+        }
+    }
+}
+
+class Hard_030_Substring_With_Concatenation_Of_All_Words {
+    class func findSubstring(# s: String?, words: [String]) -> [Int] {
+        if s == nil || count(words) == 0 {
+            return []
+        }
+        var result: [Int] = []
+        var dict1: [String: Int] = [String: Int]()
+        var dict2: [String: Int] = [String: Int]()
+        var stringLength: Int = count(s!)
+        var wordsListSize: Int = count(words)
+        var wordLength: Int = count(words[0])
+        for var i = 0; i < wordsListSize; i++ {
+            if dict1[words[i]] == nil {
+                dict1[words[i]] = 1
+            } else {
+                dict1[words[i]]! += 1
+            }
+        }
+        var s1: String
+        var s2: String
+        var counter1: Int
+        var counter2: Int
+        for var i = 0; i < wordLength; i++ {
+            counter1 = 0
+            counter2 = i
+            for var j = i; j < stringLength; j += wordLength {
+                s1 = s![j..<j+wordLength]
+                if dict1[s1] == nil {
+                    dict2 = [String: Int]()
+                    counter1 = 0
+                    counter2 = j + wordLength
+                } else if dict2[s1] == nil || dict2[s1]! < dict1[s1]! {
+                    if dict2[s1] == nil {
+                        dict2[s1] = 1
+                    } else {
+                        dict2[s1]! += 1
+                    }
+                    counter1++
+                } else {
+                    s2 = s![counter2..<counter2+wordLength]
+                    while s2 != s1 {
+                        dict2[s2]!--
+                        counter1--
+                        counter2 += wordLength
+                        s2 = s![counter2..<counter2+wordLength]
+                    }
+                    counter2 += wordLength
+                }
+                if counter1 == wordsListSize {
+                    result.append(counter2)
+                    s2 = s![counter2..<counter2+wordLength]
+                    dict2[s2]!--
+                    counter1--
+                    counter2 += wordLength
+                }
+            }
+            dict2 = [String: Int]()
+        }
+        return result
+    }
+}
---
+++ Solutions/SolutionsTests/Hard/Hard_030_Substring_With_Concatenation_Of_All_Words_Test.swift
@@ -0,0 +1,98 @@
+//
+//  Hard_030_Substring_With_Concatenation_Of_All_Words_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 5/10/15.
+//  Copyright (c) 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Hard_030_Substring_With_Concatenation_Of_All_Words_Test: XCTestCase {
+    private static let ProblemName: String = "Medium_029_Divide_Two_Integers"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    func test_001() {
+        var input: [Any?] = ["barfoothefoobarman", ["foo", "bar"]]
+        var expected: Set<Int> = Set([0, 9])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        var input: [Any?] = ["axy01bb01xy", ["01", "xy"]]
+        var expected: Set<Int> = Set([1, 7])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        var input: [Any?] = ["axy01bb01xy", ["0", "1", "x", "y"]]
+        var expected: Set<Int> = Set([1, 7])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        var input: [Any?] = ["axy01bb01xy", ["0"]]
+        var expected: Set<Int> = Set([3, 7])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        var input: [Any?] = ["__ABCDabcd__abcd_abcd_ABCD_", ["ABCD"]]
+        var expected: Set<Int> = Set([2,22])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_006() {
+        var input: [Any?] = ["__ABCDabcd__abcd_abcd_ABCD_", ["ABCD", "abcd"]]
+        var expected: Set<Int> = Set([2])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_007() {
+        var input: [Any?] = ["_ABCdefGHI___defGHIABC_ABCGHIde", ["def", "GHI", "ABC"]]
+        var expected: Set<Int> = Set([1, 13])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_008() {
+        var input: [Any?] = [nil, []]
+        var expected: Set<Int> = Set([])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_009() {
+        var input: [Any?] = ["", []]
+        var expected: Set<Int> = Set([])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_010() {
+        var input: [Any?] = ["a", []]
+        var expected: Set<Int> = Set([])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_011() {
+        var input: [Any?] = [nil, ["a"]]
+        var expected: Set<Int> = Set([])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_012() {
+        var input: [Any?] = ["abcdabcdabcdabcdabcdabcd", ["abcdabcdabcd"]]
+        var expected: Set<Int> = Set([0, 4, 8, 12])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_013() {
+        var input: [Any?] = ["abcdef", ["abcd", "bcde", "cdef"]]
+        var expected: Set<Int> = Set([])
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_014() {
+        var input: [Any?] = ["abcdefabcdcdefbcdeabcdcdef", ["abcd", "bcde", "cdef"]]
+        var expected: Set<Int> = Set([6, 10, 14])
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(var # input: [Any?], expected: Set<Int>) {
+        var expectation: XCTestExpectation = self.expectationWithDescription(Hard_030_Substring_With_Concatenation_Of_All_Words_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var result: [Int] = Hard_030_Substring_With_Concatenation_Of_All_Words.findSubstring(s: input[0] as! String?, words: input[1] as! [String])
+            assertHelper(expected == Set(result), problemName: Hard_030_Substring_With_Concatenation_Of_All_Words_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            expectation.fulfill()
+        })
+        waitForExpectationsWithTimeout(Hard_030_Substring_With_Concatenation_Of_All_Words_Test.TimeOut) { (error: NSError!) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Hard_030_Substring_With_Concatenation_Of_All_Words_Test.ProblemName, input: input, resultValue: Hard_030_Substring_With_Concatenation_Of_All_Words_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
