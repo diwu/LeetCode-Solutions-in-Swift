855c2369f40d34e36834cfc9516d29238981e3ee
Di Wu <diwufet@gmail.com>
2015-05-11 23:46:47 +0800

    hard 033

---
+++ Solutions/Solutions/Hard/Hard_033_Search_In_Rotated_Sorted_Array.swift
@@ -0,0 +1,48 @@
+/*
+
+https://leetcode.com/problems/search-in-rotated-sorted-array/
+
+#33 Search in Rotated Sorted Array 
+
+Level: hard
+
+Suppose a sorted array is rotated at some pivot unknown to you beforehand.
+
+(i.e., 0 1 2 4 5 6 7 might become 4 5 6 7 0 1 2).
+
+You are given a target value to search. If found in the array return its index, otherwise return -1.
+
+You may assume no duplicate exists in the array.
+
+Inspired by @sean hyuntaek at https://leetcode.com/discuss/5707/algorithm-using-binary-search-accepted-some-suggestions-else
+
+*/
+
+import Foundation
+
+class Hard_033_Search_In_Rotated_Sorted_Array {
+    class func search(# nums: [Int], target: Int) -> Int {
+        var start: Int = 0
+        var end: Int = count(nums) - 1
+        while start <= end {
+            var mid: Int = (start + end) / 2
+            if nums[mid] == target {
+                return mid
+            }
+            if nums[start] <= nums[mid] {
+                if nums[start] <= target && target <= nums[mid] {
+                    end = mid - 1
+                } else {
+                    start = mid + 1
+                }
+            } else {
+                if nums[mid] <= target && target <= nums[end] {
+                    start = mid + 1
+                } else {
+                    end = mid - 1
+                }
+            }
+        }
+        return -1
+    }
+}
---
+++ Solutions/SolutionsTests/Hard/Hard_033_Search_In_Rotated_Sorted_Array_Test.swift
@@ -0,0 +1,78 @@
+//
+//  Hard_033_Search_In_Rotated_Sorted_Array_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 5/11/15.
+//  Copyright (c) 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Hard_033_Search_In_Rotated_Sorted_Array_Test: XCTestCase {
+    private static let ProblemName: String = "Hard_033_Search_In_Rotated_Sorted_Array"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    func test_001() {
+        var input: [AnyObject] = [[4, 5, 6, 7, 0, 1, 2], 5]
+        var expected: Int = 1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        var input: [AnyObject] = [[4, 5, 6, 7, 0, 1, 2], 9]
+        var expected: Int = -1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        var input: [AnyObject] = [[4, 5, 6, 7, 0, 1, 2], 4]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        var input: [AnyObject] = [[4, 5, 6, 7, 0, 1, 2], 2]
+        var expected: Int = 6
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        var input: [AnyObject] = [[1], 1]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_006() {
+        var input: [AnyObject] = [[1], 9]
+        var expected: Int = -1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_007() {
+        var input: [AnyObject] = [[1, 2, 3, 4, 5, 6, 7], 4]
+        var expected: Int = 3
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_008() {
+        var input: [AnyObject] = [[1, 2, 3, 4, 5, 6, 7], 1]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_009() {
+        var input: [AnyObject] = [[1, 2, 3, 4, 5, 6, 7], 8]
+        var expected: Int = -1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_010() {
+        var input: [AnyObject] = [[1, 2, 3, 4, 5, 6, 7], 7]
+        var expected: Int = 6
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(var # input: [AnyObject], expected: Int) {
+        var expectation: XCTestExpectation = self.expectationWithDescription(Hard_033_Search_In_Rotated_Sorted_Array_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var result: Int = Hard_033_Search_In_Rotated_Sorted_Array.search(nums: input[0] as! [Int], target: input[1] as! Int)
+            assertHelper(expected == result, problemName: Hard_033_Search_In_Rotated_Sorted_Array_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            expectation.fulfill()
+        })
+        waitForExpectationsWithTimeout(Hard_033_Search_In_Rotated_Sorted_Array_Test.TimeOut) { (error: NSError!) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Hard_033_Search_In_Rotated_Sorted_Array_Test.ProblemName, input: input, resultValue: Hard_033_Search_In_Rotated_Sorted_Array_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
