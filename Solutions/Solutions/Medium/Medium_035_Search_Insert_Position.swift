17c40ae87e253f7eee25545009388ea1f340aa3c
Di Wu <diwufet@gmail.com>
2015-05-12 23:22:50 +0800

    medium 035

---
+++ Solutions/Solutions/Medium/Medium_035_Search_Insert_Position.swift
@@ -0,0 +1,41 @@
+/*
+
+https://leetcode.com/problems/search-insert-position/
+
+#35 Search Insert Position
+
+Level: medium
+
+Given a sorted array and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0
+
+Inspired by @ouchxp at https://leetcode.com/discuss/8869/simple-binary-search-solution
+
+*/
+
+import Foundation
+
+class Medium_035_Search_Insert_Position {
+    class func searchInsert(# nums: [Int], target: Int) -> Int {
+        var low: Int = 0
+        var high: Int = count(nums) - 1
+        while low <= high {
+            var mid: Int = low + (high - low)/2
+            if target < nums[mid] {
+                high = mid - 1
+            } else if target > nums[mid] {
+                low = mid + 1
+            } else {
+                return mid
+            }
+        }
+        return low
+    }
+}
---
+++ Solutions/SolutionsTests/Medium/Medium_035_Search_Insert_Position_Test.swift
@@ -0,0 +1,78 @@
+//
+//  Medium_035_Search_Insert_Position_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 5/12/15.
+//  Copyright (c) 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Medium_035_Search_Insert_Position_Test: XCTestCase {
+    private static let ProblemName: String = "Medium_035_Search_Insert_Position"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    func test_001() {
+        var input: [AnyObject] = [[1,3,5,6], 5]
+        var expected: Int = 2
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        var input: [AnyObject] = [[1,3,5,6], 2]
+        var expected: Int = 1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        var input: [AnyObject] = [[1,3,5,6], 7]
+        var expected: Int = 4
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        var input: [AnyObject] = [[1,3,5,6], 0]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        var input: [AnyObject] = [[], 999]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_006() {
+        var input: [AnyObject] = [[1], 1]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_007() {
+        var input: [AnyObject] = [[1], 0]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_008() {
+        var input: [AnyObject] = [[1], 2]
+        var expected: Int = 1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_009() {
+        var input: [AnyObject] = [[1,3,5,6], 1]
+        var expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_010() {
+        var input: [AnyObject] = [[1,3,5,6], 6]
+        var expected: Int = 3
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(var # input: [AnyObject], expected: Int) {
+        var expectation: XCTestExpectation = self.expectationWithDescription(Medium_035_Search_Insert_Position_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var result: Int = Medium_035_Search_Insert_Position.searchInsert(nums: input[0] as! [Int], target: input[1] as! Int)
+            assertHelper(expected == result, problemName: Medium_035_Search_Insert_Position_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            expectation.fulfill()
+        })
+        waitForExpectationsWithTimeout(Medium_035_Search_Insert_Position_Test.TimeOut) { (error: NSError!) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Medium_035_Search_Insert_Position_Test.ProblemName, input: input, resultValue: Medium_035_Search_Insert_Position_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
