fa392000d5ab11b907ea5d3222e40a9f8865ac38
Di Wu <diwufet@gmail.com>
2015-05-23 19:40:37 +0800

    medium 050

--- Solutions/Solutions/Medium/Medium_050_Pow_X_N.swift
+++ Solutions/Solutions/Medium/Medium_050_Pow_X_N.swift
@@ -1,13 +1,28 @@
-//
-//  Medium_050_Pow_X_N.swift
-//  Solutions
-//
-//  Created by Di Wu on 5/23/15.
-//  Copyright (c) 2015 diwu. All rights reserved.
-//
+/*
+
+https://leetcode.com/problems/powx-n/
+
+#50 Pow(x, n) 
+
+Level: medium
+
+Implement pow(x, n).
+
+Inspired by @pei heng at https://leetcode.com/discuss/17005/short-and-easy-to-understand-solution
+
+*/
 
 import Foundation
 
 struct Medium_050_Pow_X_N {
-    
+    static func myPow(var # x: Double, var n: Int) -> Double {
+        if n == 0 {
+            return 1;
+        }
+        if n < 0 {
+            n = -n;
+            x = 1/x;
+        }
+        return ( n % 2 == 0) ? myPow(x: x*x, n: n/2) : x*myPow(x: x*x, n: n/2);
+    }
 }
--- Solutions/SolutionsTests/Medium/Medium_050_Pow_X_N_Test.swift
+++ Solutions/SolutionsTests/Medium/Medium_050_Pow_X_N_Test.swift
@@ -9,5 +9,37 @@
 import XCTest
 
 class Medium_050_Pow_X_N_Test: XCTestCase {
-    
+    private static let ProblemName: String = "Medium_050_Pow_X_N"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    func test_001() {
+        var input: [Any] = [987.654, 4]
+        var expected: Double = pow(987.654, 4)
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        var input: [Any] = [987.654, 0]
+        var expected: Double = pow(987.654, 0)
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        var input: [Any] = [-987.654, 3]
+        var expected: Double = pow(-987.654, 3)
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(# input: [Any], expected: Double) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Medium_050_Pow_X_N_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            var result = Medium_050_Pow_X_N.myPow(x: input[0] as! Double, n: input[1] as! Int)
+            assertHelper(abs(expected - result) < 0.001, problemName: Medium_050_Pow_X_N_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Medium_050_Pow_X_N_Test.TimeOut) { (error: NSError!) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Medium_050_Pow_X_N_Test.ProblemName, input: input, resultValue: Medium_050_Pow_X_N_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
 }
