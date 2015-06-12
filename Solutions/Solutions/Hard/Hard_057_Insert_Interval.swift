314a853c12ee65484321d14cac711155ae7845dd
Di Wu <diwufet@163.com>
2015-06-13 01:25:24 +0800

    hard 057

---
+++ Solutions/Solutions/Hard/Hard_057_Insert_Interval.swift
@@ -0,0 +1,44 @@
+/*
+
+https://leetcode.com/discuss/3971/in-place-solution-ask-for-suggestion
+
+#57 Insert Interval
+
+Level: hard
+
+Given a set of non-overlapping intervals, insert a new interval into the intervals (merge if necessary).
+
+You may assume that the intervals were initially sorted according to their start times.
+
+Example 1:
+Given intervals [1,3],[6,9], insert and merge [2,5] in as [1,5],[6,9].
+
+Example 2:
+Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as [1,2],[3,10],[12,16].
+
+This is because the new interval [4,9] overlaps with [3,5],[6,7],[8,10].
+
+Inspired by @stellari at https://leetcode.com/discuss/3971/in-place-solution-ask-for-suggestion
+
+*/
+
+import Foundation
+
+struct Hard_057_Insert_Interval {
+    static func insert(intervals intervals: [[Int]], var newInterval: [Int]) -> [[Int]] {
+        var res: [[Int]] = []
+        for var i = 0; i < intervals.count; i++ {
+            if newInterval[0] > intervals[i][1] {
+                res.append(intervals[i])
+            } else if newInterval[1] < intervals[i][0] {
+                res.append(newInterval)
+                newInterval = intervals[i]
+            } else {
+                newInterval[0] = min(newInterval[0], intervals[i][0])
+                newInterval[1] = max(newInterval[1], intervals[i][1])
+            }
+        }
+        res.append(newInterval)
+        return res
+    }
+}
---
+++ Solutions/SolutionsTests/Hard/Hard_057_Insert_Interval_Test.swift
@@ -0,0 +1,179 @@
+//
+//  Hard_057_Insert_Interval_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 6/13/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Hard_057_Insert_Interval_Test: XCTestCase {
+    private static let ProblemName: String = "Hard_057_Insert_Interval"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    func test_001() {
+        let input0: [[Int]] = [
+            [1,3],
+            [6,9]
+        ]
+        let input1: [Int] = [
+             2,5
+        ]
+        let expected: [[Int]] = [
+            [1,5],
+            [6,9]
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_002() {
+        let input0: [[Int]] = [
+            [1,2],
+            [3,5],
+            [6,7],
+            [8,10],
+            [12,16]
+        ]
+        let input1: [Int] = [
+            4,9
+        ]
+        let expected: [[Int]] = [
+            [1,2],
+            [3,10],
+            [12,16]
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_003() {
+        let input0: [[Int]] = [
+        ]
+        let input1: [Int] = [
+        ]
+        let expected: [[Int]] = [
+            []
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_004() {
+        let input0: [[Int]] = [
+            [1,2],
+            [3,5],
+            [6,7],
+            [8,10],
+            [12,16]
+        ]
+        let input1: [Int] = [
+            1,16
+        ]
+        let expected: [[Int]] = [
+            [1,16]
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_005() {
+        let input0: [[Int]] = [
+            [1,2],
+            [3,5],
+            [6,7],
+            [8,10],
+            [12,16]
+        ]
+        let input1: [Int] = [
+            2,15
+        ]
+        let expected: [[Int]] = [
+            [1,16]
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_006() {
+        let input0: [[Int]] = [
+            [1,2],
+            [3,5],
+            [6,7],
+            [8,10],
+            [12,16]
+        ]
+        let input1: [Int] = [
+            2,17
+        ]
+        let expected: [[Int]] = [
+            [1,17]
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_007() {
+        let input0: [[Int]] = [
+            [1,2],
+            [3,5],
+            [6,7],
+            [8,10],
+            [99,100]
+        ]
+        let input1: [Int] = [
+            200, 202
+        ]
+        let expected: [[Int]] = [
+            [1,2],
+            [3,5],
+            [6,7],
+            [8,10],
+            [99,100],
+            [200,202]
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_008() {
+        let input0: [[Int]] = [
+            [3,5],
+            [6,7],
+            [8,10],
+            [99,100]
+        ]
+        let input1: [Int] = [
+             1,2
+        ]
+        let expected: [[Int]] = [
+            [1,2],
+            [3,5],
+            [6,7],
+            [8,10],
+            [99,100],
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    func test_009() {
+        let input0: [[Int]] = [
+            [3,5],
+            [6,7],
+            [8,10],
+            [99,100]
+        ]
+        let input1: [Int] = [
+            80,81
+        ]
+        let expected: [[Int]] = [
+            [3,5],
+            [6,7],
+            [8,10],
+            [80,81],
+            [99,100]
+        ]
+        asyncHelper(input0: input0, input1: input1, expected: expected)
+    }
+    private func asyncHelper(input0  input0: [[Int]], input1: [Int], expected: [[Int]]) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Hard_057_Insert_Interval_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            let result = Hard_057_Insert_Interval.insert(intervals: input0, newInterval: input1)
+            assertHelper(result == expected, problemName: Hard_057_Insert_Interval_Test.ProblemName, input: [input0, input1], resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Hard_057_Insert_Interval_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Hard_057_Insert_Interval_Test.ProblemName, input: [input0, input1], resultValue: Hard_057_Insert_Interval_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
