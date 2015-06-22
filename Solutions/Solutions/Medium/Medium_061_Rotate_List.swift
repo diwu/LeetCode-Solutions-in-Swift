ac47ed9c311d0e7e9df77067816d9860e6339f5b
Di Wu <diwufet@163.com>
2015-06-23 00:13:04 +0800

    medium 061

--- Solutions/Solutions/Medium/Medium_061_Rotate_List.swift
+++ Solutions/Solutions/Medium/Medium_061_Rotate_List.swift
@@ -1,13 +1,50 @@
-//
-//  Medium_062_Rotate_List.swift
-//  Solutions
-//
-//  Created by Di Wu on 6/22/15.
-//  Copyright © 2015 diwu. All rights reserved.
-//
+/*
+
+https://leetcode.com/problems/rotate-list/
+
+#62 Rotate List
+
+Level: medium
+
+Given a list, rotate the list to the right by k places, where k is non-negative.
+
+For example:
+Given 1->2->3->4->5->NULL and k = 2,
+return 4->5->1->2->3->NULL.
+
+Inspired by @reeclapple at https://leetcode.com/discuss/9533/share-my-java-solution-with-explanation
+
+*/
 
 import Foundation
 
-struct Medium_062_Rotate_List {
-    
+class Medium_061_Rotate_List {
+    class Node {
+        var value: Int
+        var next: Node?
+        init(value: Int, next: Node?) {
+            self.value = value
+            self.next = next
+        }
+    }
+    class func rotateRight(head head: Node?, k: Int) -> Node? {
+        if head == nil || head?.next == nil {
+            return head;
+        }
+        let dummy: Node = Node(value: 0, next: nil)
+        dummy.next = head
+        var fast: Node? = dummy
+        var slow: Node? = dummy
+        var i: Int
+        for i = 0; fast?.next != nil; i++ {
+            fast = fast?.next
+        }
+        for var j = i - k % i; j > 0; j-- {
+            slow = slow?.next
+        }
+        fast?.next = dummy.next
+        dummy.next = slow?.next
+        slow?.next = nil
+        return dummy.next
+    }
 }
--- Solutions/SolutionsTests/Medium/Medium_061_Rotate_List_Test.swift
+++ Solutions/SolutionsTests/Medium/Medium_061_Rotate_List_Test.swift
@@ -6,4 +6,89 @@
 //  Copyright © 2015 diwu. All rights reserved.
 //
 
-import Foundation
+import XCTest
+
+class Medium_061_Rotate_List_Test: XCTestCase {
+    private static let ProblemName: String = "Medium_061_Rotate_List"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    
+    private typealias Node = Medium_061_Rotate_List.Node
+    
+    func test_001() {
+        let input: [AnyObject] = [[1, 2, 3, 4, 5], 2]
+        let expected: [Int] = [4, 5, 1, 2, 3]
+        asyncHelper(input: input, expected: expected)
+    }
+    
+    func test_002() {
+        let input: [AnyObject] = [[1, 2, 3, 4, 5], 7]
+        let expected: [Int] = [4, 5, 1, 2, 3]
+        asyncHelper(input: input, expected: expected)
+    }
+    
+    func test_003() {
+        let input: [AnyObject] = [[1, 2, 3, 4, 5], 1]
+        let expected: [Int] = [5, 1, 2, 3, 4]
+        asyncHelper(input: input, expected: expected)
+    }
+    
+    func test_004() {
+        let input: [AnyObject] = [[0, 1, 2], 4]
+        let expected: [Int] = [2, 0, 1]
+        asyncHelper(input: input, expected: expected)
+    }
+    
+    func test_005() {
+        let input: [AnyObject] = [[0, 1, 2], 0]
+        let expected: [Int] = [0, 1, 2]
+        asyncHelper(input: input, expected: expected)
+    }
+    
+    func test_006() {
+        let input: [AnyObject] = [[], 99]
+        let expected: [Int] = []
+        asyncHelper(input: input, expected: expected)
+    }
+    
+    private func helper1(intArray: [Int]) -> Node? {
+        var nodeArray: [Node] = []
+        for i in intArray {
+            let node: Node = Node(value: i, next: nil)
+            nodeArray.append(node)
+        }
+        if nodeArray.count == 0 {
+            return nil
+        }
+        for i in 0..<nodeArray.count-1 {
+            nodeArray[i].next = nodeArray[i+1]
+        }
+        return nodeArray[0]
+    }
+    
+    private func helper2(head: Node?) -> [Int] {
+        var res: [Int] = []
+        var localHead = head
+        while localHead != nil {
+            res.append(localHead!.value)
+            localHead = localHead?.next
+        }
+        return res
+    }
+    
+    func asyncHelper(input  input: [AnyObject], expected: [Int]) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Medium_061_Rotate_List_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            let result: [Int] = self.helper2(Medium_061_Rotate_List.rotateRight(head: self.helper1(input[0] as! [Int]), k: input[1] as! Int))
+            assertHelper(expected == result, problemName: Medium_061_Rotate_List_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Medium_061_Rotate_List_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Medium_061_Rotate_List_Test.ProblemName, input: input, resultValue: Medium_061_Rotate_List_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
