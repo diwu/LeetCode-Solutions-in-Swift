66ab3153af97c63f595064f0cc0e924c8e8211f7
Di Wu <diwufet@163.com>
2015-06-23 00:12:37 +0800

    medium 061

---
+++ Solutions/Solutions/Medium/Medium_061_Rotate_List.swift
@@ -0,0 +1,13 @@
+//
+//  Medium_062_Rotate_List.swift
+//  Solutions
+//
+//  Created by Di Wu on 6/22/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import Foundation
+
+struct Medium_062_Rotate_List {
+    
+}
---
+++ Solutions/SolutionsTests/Medium/Medium_061_Rotate_List_Test.swift
@@ -0,0 +1,9 @@
+//
+//  Medium_061_Rotate_List_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 6/22/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import Foundation
