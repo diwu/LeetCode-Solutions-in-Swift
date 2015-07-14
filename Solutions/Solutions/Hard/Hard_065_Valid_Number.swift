94e68d08bcd0e57950af36d4b9a79ee4a0c334d8
Di Wu <diwufet@163.com>
2015-07-15 00:15:13 +0800

    hard 065

---
+++ Solutions/Solutions/Hard/Hard_065_Valid_Number.swift
@@ -0,0 +1,89 @@
+/*
+
+https://leetcode.com/problems/valid-number/
+
+#65 Valid Number 
+
+Level: hard
+
+Validate if a given string is numeric.
+
+Some examples:
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+
+Note: It is intended for the problem statement to be ambiguous. You should gather all requirements up front before implementing one.
+
+Inspired by @Echo-yang at https://leetcode.com/discuss/13691/c-my-thought-with-dfa
+
+*/
+
+import Foundation
+
+private extension String {
+    subscript (index: Int) -> Character {
+        return self[advance(self.startIndex, index)]
+    }
+    mutating func removeAtIndex(index: Int) -> Character {
+        return self.removeAtIndex(advance(self.startIndex, index))
+    }
+}
+
+struct Hard_065_Valid_Number {
+    static func isNumber(var s: String) -> Bool {
+        var state = 0
+        var flag = 0
+        while s[0] == " " {
+            s.removeAtIndex(0)
+        }
+        while s[s.characters.count-1] == " " {
+            s.removeAtIndex(s.characters.count-1)
+        }
+        let digits: [Character: Int] = [
+            "0": 0,
+            "1": 1,
+            "2": 2,
+            "3": 3,
+            "4": 4,
+            "5": 5,
+            "6": 6,
+            "7": 7,
+            "8": 8,
+            "9": 9,
+        ]
+        for var i = 0; i < s.characters.count; i++ {
+            if digits[s[i]] != nil {
+                flag = 1
+                if state <= 2 {
+                    state = 2
+                } else {
+                    state = state <= 5 ? 5 : 7
+                }
+            } else if s[i] == "+" || s[i] == "-" {
+                if state == 0 || state == 3 {
+                    state++
+                } else {
+                    return false
+                }
+            } else if "." == s[i] {
+                if state <= 2 {
+                    state = 6
+                } else {
+                    return false
+                }
+            } else if "e" == s[i] {
+                if flag == 1 && (state == 2 || state == 6 || state == 7) {
+                    state = 3
+                } else {
+                    return false
+                }
+            } else {
+                return false
+            }
+        }
+        return (state == 2 || state == 5 || (flag == 1 && state == 6) || state == 7)
+    }
+}
---
+++ Solutions/SolutionsTests/Hard/Hard_065_Valid_Number_Test.swift
@@ -0,0 +1,160 @@
+//
+//  Hard_065_Valid_Number_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 7/14/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Hard_065_Valid_Number_Test: XCTestCase {
+    private static let ProblemName: String = "Hard_065_Valid_Number"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value * 100
+    func test_001() {
+        let input: String = "+1"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        let input: String = "-23432"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        let input: String = "123"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        let input: String = "+2.4e+12"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        let input: String = "3e9"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_006() {
+        let input: String = "1."
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_007() {
+        let input: String = "-42."
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_008() {
+        let input: String = "0."
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_009() {
+        let input: String = ".0"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_010() {
+        let input: String = "."
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_011() {
+        let input: String = "+12.23"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_012() {
+        let input: String = "87."
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_013() {
+        let input: String = "132"
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_014() {
+        let input: String = "+ 1"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_015() {
+        let input: String = "-23+432"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_016() {
+        let input: String = "e123"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_017() {
+        let input: String = "+2.4e+1.2"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_018() {
+        let input: String = "3ee9"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_019() {
+        let input: String = "1.."
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_020() {
+        let input: String = " --42."
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_021() {
+        let input: String = "0.a"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_022() {
+        let input: String = "b.0"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_023() {
+        let input: String = ".e"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_024() {
+        let input: String = " +12.23  "
+        let expected: Bool = true
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_025() {
+        let input: String = "87.b"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_026() {
+        let input: String = "13 2"
+        let expected: Bool = false
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(input input: String, expected: Bool) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Hard_065_Valid_Number_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            let result = Hard_065_Valid_Number.isNumber(input)
+            assertHelper(result == expected, problemName: Hard_065_Valid_Number_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Hard_065_Valid_Number_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Hard_065_Valid_Number_Test.ProblemName, input: input, resultValue: Hard_065_Valid_Number_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
