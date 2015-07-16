ef2c5be569aa2215d1b78b01bc4371d6978c6c79
Di Wu <diwufet@163.com>
2015-07-16 23:06:45 +0800

    easy 066

---
+++ Solutions/Solutions/Easy/Easy_066_Plus_One.swift
@@ -0,0 +1,33 @@
+/*
+
+https://leetcode.com/problems/plus-one/
+
+#66 Plus One
+
+Level: easy
+
+Given a non-negative number represented as an array of digits, plus one to the number.
+
+The digits are stored such that the most significant digit is at the head of the list.
+
+Inspired by @zezedi at https://leetcode.com/discuss/14616/is-it-a-simple-code-c
+
+*/
+
+import Foundation
+
+struct Easy_066_Plus_One {
+    static func plusOne(inout digits: [Int]) {
+        let n = digits.count
+        for var i = n - 1; i >= 0; i-- {
+            if digits[i] == 9 {
+                digits[i] = 0
+            } else {
+                digits[i]++
+                return
+            }
+        }
+        digits[0] = 1
+        digits.append(0)
+    }
+}
---
+++ Solutions/SolutionsTests/Easy/Easy_066_Plus_One_Test.swift
@@ -0,0 +1,51 @@
+//
+//  Easy_066_Plus_One_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 7/16/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Easy_066_Plus_One_Test: XCTestCase {
+    private static let ProblemName: String = "Easy_066_Plus_One"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value * 100
+    func test_001() {
+        var input: [Int] = [1, 9, 9]
+        let expected: [Int] = [2, 0, 0]
+        asyncHelper(input: &input, expected: expected)
+    }
+    func test_002() {
+        var input: [Int] = [0]
+        let expected: [Int] = [1]
+        asyncHelper(input: &input, expected: expected)
+    }
+    func test_003() {
+        var input: [Int] = [9]
+        let expected: [Int] = [1, 0]
+        asyncHelper(input: &input, expected: expected)
+    }
+    func test_004() {
+        var input: [Int] = [9, 9]
+        let expected: [Int] = [1, 0, 0]
+        asyncHelper(input: &input, expected: expected)
+    }
+    private func asyncHelper(inout input input: [Int], expected: [Int]) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Easy_066_Plus_One_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            Easy_066_Plus_One.plusOne(&input)
+            let result = input
+            assertHelper(result == expected, problemName: Easy_066_Plus_One_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Easy_066_Plus_One_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Easy_066_Plus_One_Test.ProblemName, input: input, resultValue: Easy_066_Plus_One_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
