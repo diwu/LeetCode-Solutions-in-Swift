2c26672366e664ae132d6c9b5e5d67f430fd6e4f
Di Wu <test@test.com>
2015-07-30 11:17:40 +0800

    hard 072

---
+++ Solutions/Solutions/Hard/Hard_072_Edit_Distance.swift
@@ -0,0 +1,51 @@
+/*
+
+https://leetcode.com/problems/edit-distance/
+
+#72 Edit Distance
+
+Given two words word1 and word2, find the minimum number of steps required to convert word1 to word2. (each operation is counted as 1 step.)
+
+You have the following 3 operations permitted on a word:
+
+a) Insert a character
+b) Delete a character
+c) Replace a character
+
+Inspired by @eaglesky1990 at https://leetcode.com/discuss/10426/my-o-mn-time-and-o-n-space-solution-using-dp-with-explanation
+
+*/
+
+import Foundation
+
+private extension String {
+    subscript (index: Int) -> Character {
+        return self[advance(self.startIndex, index)]
+    }
+}
+
+struct Hard_072_Edit_Distance {
+    static func minDistance(word1 word1: String, word2: String) -> Int {
+        let len1 = word1.characters.count
+        let len2 = word2.characters.count
+        var buf: [Int] = Array(count: len2 + 1, repeatedValue: 0)
+        for var j = 1; j <= len2; j++ {
+            buf[j] = j
+        }
+        for var i = 1; i <= len1; i++ {
+            var prev = i
+            for var j = 1; j <= len2; j++ {
+                var curr: Int
+                if word1[i-1] == word2[j-1] {
+                    curr = buf[j-1]
+                } else {
+                    curr = min(buf[j-1], prev, buf[j]) + 1
+                }
+                buf[j-1] = prev
+                prev = curr
+            }
+            buf[len2] = prev
+        }
+        return buf[len2]
+    }
+}
---
+++ Solutions/SolutionsTests/Hard/Hard_072_Edit_Distance_Test.swift
@@ -0,0 +1,75 @@
+//
+//  Hard_072_Edit_Distance_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 7/30/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Hard_072_Edit_Distance_Test: XCTestCase {
+    private static let ProblemName: String = "Hard_072_Edit_Distance"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value * 10
+    func test_001() {
+        let input: [String] = ["horse", "ros"]
+        let expected: Int = 3
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        let input: [String] = ["abcdxabcde", "abcdeabcdx"]
+        let expected: Int = 2
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        let input: [String] = ["a", "b"]
+        let expected: Int = 1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        let input: [String] = ["a", "a"]
+        let expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        let input: [String] = ["", ""]
+        let expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_006() {
+        let input: [String] = ["a", "aa"]
+        let expected: Int = 1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_007() {
+        let input: [String] = ["b", "aa"]
+        let expected: Int = 2
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_008() {
+        let input: [String] = ["ab", "a"]
+        let expected: Int = 1
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_009() {
+        let input: [String] = ["ab", "ba"]
+        let expected: Int = 2
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(input input: [String], expected: Int) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Hard_072_Edit_Distance_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            let result = Hard_072_Edit_Distance.minDistance(word1: input[0], word2: input[1])
+            assertHelper(result == expected, problemName: Hard_072_Edit_Distance_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Hard_072_Edit_Distance_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Hard_072_Edit_Distance_Test.ProblemName, input: input, resultValue: Hard_072_Edit_Distance_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
