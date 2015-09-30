f5a8bd76f748f93736e1307f96eca4d032210145
Di Wu <test@test.com>
2015-09-30 11:45:24 +0800

    hard 084

---
+++ Solutions/Solutions/Hard/Hard_084_Largest_Rectangle_In_Histogram.swift
@@ -0,0 +1,54 @@
+/*
+
+https://leetcode.com/problems/largest-rectangle-in-histogram/
+
+#84 Largest Rectangle in Histogram
+
+Given n non-negative integers representing the histogram's bar height where the width of each bar is 1, find the area of largest rectangle in the histogram.
+
+
+Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
+
+
+The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+For example,
+Given height = [2,1,5,6,2,3],
+return 10.
+
+Inspired by http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+
+*/
+
+import Foundation
+
+struct Hard_084_Largest_Rectangle_In_Histogram {
+    static func largestRectangleArea(heights: [Int]) -> Int {
+        var stack:[Int] = []
+        var max_area = 0
+        var top_of_stack: Int
+        var area_with_top: Int
+        var i = 0
+        while i < heights.count {
+            if stack.isEmpty || heights[stack.last!] <= heights[i] {
+                stack.append(i++)
+            } else {
+                top_of_stack = stack.last!
+                stack.removeLast()
+                area_with_top = heights[top_of_stack] * (stack.isEmpty ? i : i - stack.last! - 1)
+                if max_area < area_with_top {
+                    max_area = area_with_top
+                }
+            }
+        }
+        while stack.isEmpty == false {
+            top_of_stack = stack.last!
+            stack.removeLast()
+            area_with_top = heights[top_of_stack] * (stack.isEmpty ? i : i - stack.last! - 1)
+            if max_area < area_with_top {
+                max_area = area_with_top
+            }
+        }
+        return max_area
+    }
+}
---
+++ Solutions/SolutionsTests/Hard/Hard_084_Largest_Rectangle_In_Histogram_Test.swift
@@ -0,0 +1,65 @@
+//
+//  Hard_084_Largest_Rectangle_In_Histogram_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 9/30/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Hard_084_Largest_Rectangle_In_Histogram_Test: XCTestCase {
+    private static let ProblemName: String = "Hard_084_Largest_Rectangle_In_Histogram"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
+    func test_001() {
+        let input: [Int] = [2,1,5,6,2,3]
+        let expected: Int = 10
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        let input: [Int] = []
+        let expected: Int = 0
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        let input: [Int] = [7,4]
+        let expected: Int = 8
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        let input: [Int] = [6, 2, 5, 4, 5, 1, 6]
+        let expected: Int = 12
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        let input: [Int] = [1, 3, 5, 8, 5, 4, 6, 7, 5, 2, 1, 0]
+        let expected: Int = 28
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_006() {
+        let input: [Int] = [3,2,3]
+        let expected: Int = 6
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_007() {
+        let input: [Int] = [6, 2, 5, 4, 5, 2, 6]
+        let expected: Int = 14
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(input input: [Int], expected: Int) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Hard_084_Largest_Rectangle_In_Histogram_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            let result = Hard_084_Largest_Rectangle_In_Histogram.largestRectangleArea(input)
+            assertHelper(result == expected, problemName: Hard_084_Largest_Rectangle_In_Histogram_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Hard_084_Largest_Rectangle_In_Histogram_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Hard_084_Largest_Rectangle_In_Histogram_Test.ProblemName, input: input, resultValue: Hard_084_Largest_Rectangle_In_Histogram_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
