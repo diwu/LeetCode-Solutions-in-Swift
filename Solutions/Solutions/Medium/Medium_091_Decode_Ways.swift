aab3255c0a12a9c1358a05df2490e8d155d71c9f
Di Wu <test@test.com>
2015-10-23 18:12:46 +0800

    Medium 091

---
+++ Solutions/Solutions/Medium/Medium_091_Decode_Ways.swift
@@ -0,0 +1,55 @@
+/*
+
+https://leetcode.com/problems/decode-ways/
+
+#91 Decode Ways
+
+Level: medium
+
+A message containing letters from A-Z is being encoded to numbers using the following mapping:
+
+'A' -> 1
+'B' -> 2
+...
+'Z' -> 26
+Given an encoded message containing digits, determine the total number of ways to decode it.
+
+For example,
+Given encoded message "12", it could be decoded as "AB" (1 2) or "L" (12).
+
+The number of ways decoding "12" is 2.
+
+Inspired by @manky at https://leetcode.com/discuss/8527/dp-solution-java-for-reference
+
+*/
+
+import Foundation
+
+private extension String {
+    subscript (index: Int) -> Character {
+        return self[self.startIndex.advancedBy(index)]
+    }
+    subscript (range: Range<Int>) -> String {
+        return self[self.startIndex.advancedBy(range.startIndex)..<self.startIndex.advancedBy(range.endIndex, limit: self.endIndex)]
+    }
+}
+
+struct Medium_091_Decode_Ways {
+    static func numDecodings(s: String) -> Int {
+        let n: Int = s.characters.count
+        if n == 0 {
+            return 0
+        }
+        var memo: [Int] = Array<Int>(count: n+1, repeatedValue: 0)
+        memo[n] = 1
+        memo[n-1] = s[n-1] != "0" ? 1 : 0
+        for var i = n-2; i >= 0; i-- {
+            if s[i] == "0" {
+                continue
+            } else {
+                memo[i] = Int(s[i..<i+2]) <= 26 ? memo[i+1]+memo[i+2] : memo[i+1]
+            }
+        }
+        return memo[0]
+    }
+}
---
+++ Solutions/SolutionsTests/Medium/Medium_091_Decode_Ways_Test.swift
@@ -0,0 +1,55 @@
+//
+//  Medium_091_Decode_Ways_Test.swift
+//  Solutions
+//
+//  Created by Di Wu on 10/23/15.
+//  Copyright © 2015 diwu. All rights reserved.
+//
+
+import XCTest
+
+class Medium_091_Decode_Ways_Test: XCTestCase {
+    private static let ProblemName: String = "Medium_091_Decode_Ways"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value * 100
+    func test_001() {
+        let input: String = "12"
+        let expected: Int = 2
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_002() {
+        let input: String = "125"
+        let expected: Int = 3
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_003() {
+        let input: String = "127"
+        let expected: Int = 2
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_004() {
+        let input: String = "262"
+        let expected: Int = 2
+        asyncHelper(input: input, expected: expected)
+    }
+    func test_005() {
+        let input: String = "212"
+        let expected: Int = 3
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(input input: String, expected: Int) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Medium_091_Decode_Ways_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            let result = Medium_091_Decode_Ways.numDecodings(input)
+            assertHelper(result == expected, problemName: Medium_091_Decode_Ways_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Medium_091_Decode_Ways_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Medium_091_Decode_Ways_Test.ProblemName, input: input, resultValue: Medium_091_Decode_Ways_Test.TimeOutName, expectedValue: expected)
+            }
+        }
+    }
+}
