bfdddcbd7549f78e4542d2db3f512ac26eee3e17
Di Wu <diwufet@163.com>
2015-11-03 23:27:15 +0800

    Update Medium 017 Test Cases

--- Solutions/SolutionsTests/Medium/Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.swift
+++ Solutions/SolutionsTests/Medium/Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.swift
@@ -10,36 +10,49 @@ import XCTest
 
 class Medium_017_Letter_Combinations_Of_A_Phone_Number_Test: XCTestCase {
 
-    let ProblemName: String = "Medium_017_Letter_Combinations_Of_A_Phone_Number"
+    private static let ProblemName: String = "Medium_017_Letter_Combinations_Of_A_Phone_Number"
+    private static let TimeOutName = ProblemName + Default_Timeout_Suffix
+    private static let TimeOut = Default_Timeout_Value
     
     func test_001() {
         let input: String = "23"
-        let result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
-        let expected: Set<String> = Set(["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"])
-        assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
+        let expected = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+        asyncHelper(input: input, expected: expected)
     }
     func test_002() {
         let input: String = ""
-        let result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
-        let expected: Set<String> = Set([""])
-        assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
+        let expected = [""]
+        asyncHelper(input: input, expected: expected)
     }
     func test_003() {
         let input: String = "2"
-        let result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
-        let expected: Set<String> = Set(["a", "b", "c"])
-        assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
+        let expected = ["a", "b", "c"]
+        asyncHelper(input: input, expected: expected)
     }
     func test_004() {
         let input: String = "023"
-        let result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
-        let expected: Set<String> = Set(["0ad", "0ae", "0af", "0bd", "0be", "0bf", "0cd", "0ce", "0cf"])
-        assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
+        let expected = ["0ad", "0ae", "0af", "0bd", "0be", "0bf", "0cd", "0ce", "0cf"]
+        asyncHelper(input: input, expected: expected)
     }
     func test_005() {
         let input: String = "210"
-        let result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
-        let expected: Set<String> = Set(["a10", "b10", "c10"])
-        assertHelper(result == expected, problemName: ProblemName, input: input, resultValue: result, expectedValue: expected)
+        let expected = ["a10", "b10", "c10"]
+        asyncHelper(input: input, expected: expected)
+    }
+    private func asyncHelper(input input: String, expected: [String]) {
+        weak var expectation: XCTestExpectation? = self.expectationWithDescription(Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.TimeOutName)
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), { () -> Void in
+            let result: Set<String> = Set(Medium_017_Letter_Combinations_Of_A_Phone_Number.letterCombinations(input))
+            let expectedSet: Set<String> = Set(expected)
+            assertHelper(result == expectedSet, problemName: Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            if let unwrapped = expectation {
+                unwrapped.fulfill()
+            }
+        })
+        waitForExpectationsWithTimeout(Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.TimeOut) { (error: NSError?) -> Void in
+            if error != nil {
+                assertHelper(false, problemName: Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.ProblemName, input: input, resultValue: Medium_017_Letter_Combinations_Of_A_Phone_Number_Test.TimeOutName, expectedValue: expected)
+            }
+        }
     }
 }
