d2c1017ee3df9ffbfd04012fb5752f53f2d7518e
Di Wu <diwup@foxmail.com>
2017-02-06 11:16:07 +0800

    Silence new syntax warnings

--- Solutions/SolutionsTests/Hard/Hard_032_Longest_Valid_Parentheses_Test.swift
+++ Solutions/SolutionsTests/Hard/Hard_032_Longest_Valid_Parentheses_Test.swift
@@ -56,14 +56,14 @@ class Hard_032_Longest_Valid_Parentheses_Test: XCTestCase {
         weak var expectation: XCTestExpectation? = self.expectation(description: Hard_032_Longest_Valid_Parentheses_Test.TimeOutName)
         DispatchQueue.global(qos: DispatchQoS.QoSClass.default).async(execute: { () -> Void in
             let result: Int = Hard_032_Longest_Valid_Parentheses.longestValidParentheses(input)
-            assertHelper(expected == result, problemName: Hard_032_Longest_Valid_Parentheses_Test.ProblemName, input: input, resultValue: result, expectedValue: expected)
+            assertHelper(expected == result, problemName: Hard_032_Longest_Valid_Parentheses_Test.ProblemName, input: input as Any, resultValue: result, expectedValue: expected)
             if let unwrapped = expectation {
                 unwrapped.fulfill()
             }
         })
         waitForExpectations(timeout: Hard_032_Longest_Valid_Parentheses_Test.TimeOut) { (error: Error?) -> Void in
             if error != nil {
-                assertHelper(false, problemName: Hard_032_Longest_Valid_Parentheses_Test.ProblemName, input: input, resultValue: Hard_032_Longest_Valid_Parentheses_Test.TimeOutName, expectedValue: expected)
+                assertHelper(false, problemName: Hard_032_Longest_Valid_Parentheses_Test.ProblemName, input: input as Any, resultValue: Hard_032_Longest_Valid_Parentheses_Test.TimeOutName, expectedValue: expected)
             }
         }
     }
