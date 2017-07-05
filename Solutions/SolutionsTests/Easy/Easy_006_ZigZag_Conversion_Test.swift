ac4ad46a00c277513b66f843b03e67c52ebe4a50
Di Wu <diwufet@163.com>
2017-07-05 21:56:55 +0800

    3 more test cases for 006.

--- Solutions/SolutionsTests/Easy/Easy_006_ZigZag_Conversion_Test.swift
+++ Solutions/SolutionsTests/Easy/Easy_006_ZigZag_Conversion_Test.swift
@@ -18,6 +18,33 @@ class Easy_006_ZigZag_Conversion_Test: XCTestCase {
         let result = Easy_006_ZigZag_Conversion.convert(s: input[0] as! String, nRows: input[1] as! Int)
         assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
     }
+    
+    func testCase002() {
+        var input: [Any] = ["PAYPALISHIRING", 1]
+        let expected: String = "PAYPALISHIRING"
+        let result = Easy_006_ZigZag_Conversion.convert(s: input[0] as! String, nRows: input[1] as! Int)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
+    }
+    
+    func testCase003() {
+        var input: [Any] = ["", 3]
+        let expected: String = ""
+        let result = Easy_006_ZigZag_Conversion.convert(s: input[0] as! String, nRows: input[1] as! Int)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
+    }
+    
+    func testCase004() {
+        /*
+         P   I   N
+         A L S I G
+         Y A H R
+         P   I
+         */
+        var input: [Any] = ["PAYPALISHIRING", 4]
+        let expected: String = "PINALSIGYAHRPI"
+        let result = Easy_006_ZigZag_Conversion.convert(s: input[0] as! String, nRows: input[1] as! Int)
+        assertHelper(result == expected, problemName: ProbleName, input: input, resultValue: result, expectedValue: expected)
+    }
 }
 
 /*
