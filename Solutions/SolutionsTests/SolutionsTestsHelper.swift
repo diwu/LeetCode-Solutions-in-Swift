3293db043ab8cc260f78410ad81e9dd3536ddf8e
Di Wu <diwufet@163.com>
2017-08-13 15:46:07 +0800

    Improve infrastructure.

--- Solutions/SolutionsTests/SolutionsTestsHelper.swift
+++ Solutions/SolutionsTests/SolutionsTestsHelper.swift
@@ -11,6 +11,30 @@ import XCTest
 let Default_Timeout_Value = 0.01
 let Default_Timeout_Suffix: String = " Time Out"
 
+protocol SolutionsTestCase {
+    func problemName() -> String
+    func timeOutName() -> String
+    func timeOut() -> TimeInterval
+    func timeOutSuffix() -> String
+    func serialQueue() -> DispatchQueue
+}
+
+extension SolutionsTestCase {
+    func timeOut() -> TimeInterval {
+        return 0.01
+    }
+    func timeOutSuffix() -> String {
+        return " Time Out"
+    }
+    func timeOutName() -> String {
+        return problemName() + timeOutSuffix()
+    }
+    func serialQueue() -> DispatchQueue {
+        let queue = DispatchQueue(label: problemName(), qos: .`default`, attributes: [], autoreleaseFrequency: .workItem, target: nil)
+        return queue
+    }
+}
+
 func assertHelper(_ expression: @autoclosure () -> Bool, problemName: String , input: Any, resultValue: Any, expectedValue: Any) {
     XCTAssert(expression(), "\n====================================\nProblem: \(problemName)\n====================================\nInput: \"\(input)\"\n====================================\nExpected: \"\(expectedValue)\"\n====================================\nResult: \"\(resultValue)\"\n====================================\n")
 }
