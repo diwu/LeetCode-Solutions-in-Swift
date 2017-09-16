79571f346a8581f6c884c06d45d361255f7cb4dd
Di Wu <diwufet@163.com>
2017-09-16 18:15:41 +0800

    Code rewrite for 093.

--- Solutions/Solutions/Medium/Medium_093_Restore_IP_Addresses.swift
+++ Solutions/Solutions/Medium/Medium_093_Restore_IP_Addresses.swift
@@ -1,63 +1,54 @@
 /*
-
-https://leetcode.com/problems/restore-ip-addresses/
-
-#93 Restore IP Addresses
-
-Level: medium
-
-Given a string containing only digits, restore it by returning all possible valid IP address combinations.
-
-For example:
-Given "25525511135",
-
-return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
-
-Inspired by @fiona_mao at https://leetcode.com/discuss/12790/my-code-in-java
-
-*/
+ 
+ https://leetcode.com/problems/restore-ip-addresses/
+ 
+ #93 Restore IP Addresses
+ 
+ Level: medium
+ 
+ Given a string containing only digits, restore it by returning all possible valid IP address combinations.
+ 
+ For example:
+ Given "25525511135",
+ 
+ return ["255.255.11.135", "255.255.111.35"]. (Order does not matter)
+ 
+ Inspired by @fiona_mao at https://leetcode.com/discuss/12790/my-code-in-java
+ 
+ */
 
 import Foundation
-
-private extension String {
-    subscript (range: Range<Int>) -> String {
-        return String(self[self.characters.index(self.startIndex, offsetBy: range.lowerBound)..<self.characters.index(self.startIndex, offsetBy: range.upperBound, limitedBy: self.endIndex)!])
-    }
-}
 
 struct Medium_093_Restore_IP_Addresses {
     // t = O(1), s = O(1)
     static func restoreIpAddresses(_ s: String) -> [String] {
-        var res = Array<String>()
-        let len: Int = s.characters.count
-        for i in 1 ..< min(4, len - 2) {
-            for j in i + 1 ..< min(i+4, len-1) {
-                for k in j + 1 ..< min(j + 4, len) {
-                    let s0: String = s[0..<i]
-                    let s1: String = s[i..<j]
-                    let s2: String = s[j..<k]
-                    let s3: String = s[k..<len]
-                    if isValid(s0) && isValid(s1) && isValid(s2) && isValid(s3) {
-                        res.append("\(s0).\(s1).\(s2).\(s3)")
-                    }
+        let chars = [Character](s.characters)
+        let len = chars.count
+        guard len >= 4 else { return [] }
+        var result = [String]()
+        for i in 1...min(3, len-3) {
+            for j in i...min(i+3, len-2) {
+                for k in j...min(j+3, len-1) {
+                    let s0 = String(chars[0..<i])
+                    guard validate(s0) == true else { continue }
+                    let s1 = String(chars[i..<j])
+                    guard validate(s1) == true else { continue }
+                    let s2 = String(chars[j..<k])
+                    guard validate(s2) == true else { continue }
+                    let s3 = String(chars[k..<len])
+                    guard validate(s3) == true else { continue }
+                    result.append("\(s0).\(s1).\(s2).\(s3)")
                 }
             }
         }
-        return res
+        return result
     }
-    private static func isValid(_ s: String) -> Bool {
-        if s.characters.count > 3 {
-            return false
-        }
-        if s.characters.count == 0 {
-            return false
-        }
-        if s.characters.count > 1 && s.characters.first == "0" {
-            return false
-        }
-        if Int(s)! > 255 {
-            return false
-        }
-        return true
+    static func validate(_ s: String) -> Bool {
+        let chars = [Character](s)
+        if chars.count > 1 && chars[0] == "0" { return false }
+        let v = Int(s) ?? Int.max
+        if v < 0 || v > 255 { return false }
+        else { return true }
     }
 }
+
